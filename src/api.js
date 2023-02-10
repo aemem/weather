c7f2556fc5cc3b27b0f0b0d2af1b037a0749ba09
AM <anamoliner.marques@gmail.com>
2023-02-10 01:22:04 +0100

    .

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '67530c5a1bmsh252bdbbd5c20ae7p15f068jsn3552bb127085',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "9a151782904b8b2fa4390794dfc9e7d0";
